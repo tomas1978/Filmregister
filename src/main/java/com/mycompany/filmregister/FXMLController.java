abe6bb2f7b6b6434d62d8cc51d7e01e4664af8bd
tomas1978 <tomas.nilsson78@gmail.com>
2019-11-20 23:45:49 +0100

    Börjat skriva kod för att visa filmerna i Listview-rutan

---
+++ src/main/java/com/mycompany/filmregister/FXMLController.java
@@ -0,0 +1,88 @@
+package com.mycompany.filmregister;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+
+public class FXMLController implements Initializable {
+
+    FilmManager minaFilmer = new FilmManager();
+    
+    @FXML
+    private TextField titelTextBox;
+
+    @FXML
+    private TextField regissörTextBox;
+
+    @FXML
+    private TextField längdTextBox;
+
+    @FXML
+    private TextField betygTextBox;
+    
+    @FXML
+    private ListView<Film> filmerListView;
+    
+    @FXML
+    void nyFilmAction(ActionEvent event) throws IOException {
+       
+        Parent root = FXMLLoader.load(getClass().getResource("/fxml/nyFilmScene.fxml"));
+        Stage s = new Stage();
+        Scene scene = new Scene(root);
+        scene.getStylesheets().add("/styles/Styles.css");
+        
+        s.setTitle("Registrera ny film");
+        s.setScene(scene);
+        s.show();
+    }
+
+    @FXML
+    void searchAction(ActionEvent event) {
+        
+    }
+
+    @FXML
+    void visaAllaAction(ActionEvent event) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("/fxml/visaFilmerScene.fxml"));
+        
+        filmerListView.setItems(minaFilmer.getObsFilmLista());
+        
+        Stage s = new Stage();
+        Scene scene = new Scene(root);
+        scene.getStylesheets().add("/styles/Styles.css");
+        
+        s.setTitle("Visa filmer");
+        s.setScene(scene);
+        s.show();
+    }
+    
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        System.out.println("Init run");
+        filmerListView = new ListView<Film>();
+    }    
+    
+    @FXML
+    void läggTillFilmAction(ActionEvent event) {
+        Film nyFilm = new Film(titelTextBox.getText(), regissörTextBox.getText(), 
+                                Integer.parseInt(längdTextBox.getText()), 
+                                Float.parseFloat(betygTextBox.getText()));
+        minaFilmer.läggTillFilm(nyFilm);
+        System.out.println(minaFilmer);
+        //filmerListView.setItems(minaFilmer.getObsFilmLista());
+    }
+    
+    @FXML
+    void updateMovieListAction(ActionEvent event) {
+         filmerListView.setItems(minaFilmer.getObsFilmLista());
+    }
+}
--- src/main/java/com/mycompany/filmregister/FilmManager.java
+++ src/main/java/com/mycompany/filmregister/FilmManager.java
@@ -7,27 +7,37 @@ package com.mycompany.filmregister;
 
 import java.util.ArrayList;
 import java.util.List;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 
 /**
  *
  * @author tomas
  */
 public class FilmManager {
-    private List<Film> filmlista = new ArrayList<>();
+    private final List<Film> filmlista = new ArrayList<>();
+    private ObservableList<Film> obsFilmLista;
     
     public void läggTillFilm(Film nyFilm) {
         filmlista.add(nyFilm);
+        obsFilmLista.add(nyFilm);
+    }
+
+    public ObservableList<Film> getObsFilmLista() {
+        return obsFilmLista;
+    }
+
+    public void setObsFilmLista(ObservableList<Film> obsFilmLista) {
+        this.obsFilmLista = obsFilmLista;
+    }
+
+    public FilmManager() {
+        this.obsFilmLista = FXCollections.observableList(filmlista);
     }
 
     public List<Film> getFilmlista() {
         return filmlista;
     }
-
-    public void setFilmlista(List<Film> filmlista) {
-        this.filmlista = filmlista;
-    }
-    
-    
 
     @Override
     public String toString() {
